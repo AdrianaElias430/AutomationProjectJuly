f914e7d109a713953c4b776dbc46fb0ca6989450
Adriana <adelira_6531@hotmail.com>
2021-07-28 20:07:03 -0500

    new changes

--- AutomationProjectJuly/src/test/java/com/java/Multiplicacion.java
+++ AutomationProjectJuly/src/test/java/com/java/Multiplicacion.java
@@ -2,4 +2,14 @@ package com.java;
 
 public class Multiplicacion {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int a = 14; 
+			int b = 8; 
+			int c = 12;
+		    int result = a * b * c;
+			System.out.println(result);
+	}
+
 }
